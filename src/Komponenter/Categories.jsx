7383f5437d6ddc850a4553bf35a05df836d95b90
naathljee <146845810+naathljee@users.noreply.github.com>
2024-04-01 22:35:06 +0200

    Legger inn komponenter filer og api i js versjon

---
+++ src/Komponenter/Categories.jsx
@@ -0,0 +1,11 @@
+export default function kategorier (){
+    const Kategorier = ["title","publish_year","publish_place", "lenguage"]
+    return(
+        <>
+        <h1>Kategorier</h1>
+        <ul>
+        {Kategorier.map((kategori, index) => <li key={kategori+index}><Link to={category}>{category}</Link> </li>)}
+        </ul>
+        </>
+    )
+}
