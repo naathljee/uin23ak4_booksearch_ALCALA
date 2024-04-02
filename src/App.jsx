07f2c406c835f22a67fb570e91d9f7c0b7ac7a29
naathljee <146845810+naathljee@users.noreply.github.com>
2024-04-02 17:12:49 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import { Route,Routes } from 'react-router-dom'
 import './App.css'
 import { booksApi } from './assets/booksApi'
-import Bookcard from './Komponenter/bookcard'
+import Bookcard from './Komponenter/Bookcard'
+
+
 function App() {
   console.log(booksApi)
 
 
   return (
   <>
-  <div className='app'>
+  <div className='App'>
     {booksApi.map((book) =>
     <Bookcard key={book.docs} bName={book.title} bAuthor={book.author_name} bPublish={book.publish_year} bRate={book.ratings_count} />
 
