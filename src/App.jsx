7e7332e8021fadd46cf31af523bc095882b81095
naathljee <146845810+naathljee@users.noreply.github.com>
2024-04-02 22:09:15 +0200

    VBNHJ

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 //import {booksApi} from '../src/assets/booksApi.json'
 import Bookcard from './Komponenter/Bookcard'
 import Knapp from './Komponenter/Knapp'
+import Visning from './Komponenter/Visning'
+import Home from './Komponenter/Home'
 
 
 function App() {
@@ -22,9 +24,11 @@ function App() {
   }, [])
 
 
-  return (<>
-  </>
-  )
-}
-
-export default App
+  return (
+  <Routes>
+    <Route element={<Visning/>}/>
+      <Route element ={<Home/>}/>
+      <Route path=':slug' element={Bookcard}/>
+ertyuio0p+
+´¨_
+7
--- src/Komponenter/Home.jsx
+++ src/Komponenter/Home.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom'
 
-export default function Categories (){
+export default function Home (){
         const {slug} = useParams ()
         return <h1>{slug}</h1>
         
