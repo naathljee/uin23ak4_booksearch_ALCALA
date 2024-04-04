ed0c283c6a8f36e0c33a69a2995ff0912bd5984a
naathljee <146845810+naathljee@users.noreply.github.com>
2024-04-05 01:21:11 +0200

    i tillfelle rottefelle

--- src/Komponenter/Visning.jsx
+++ src/Komponenter/Visning.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import Home from './Home';
 import Searchresult from './Searchresult';
 
+//useStaten min passer på at søkeresultatet starter som null
 function Visning({ books }) {
   const [searchResult, setSearchResult] = useState(null);
-
+//fetcher Api-et
   const handleSearch = async (searchText) => {
     try {
       const response = await fetch(`https://openlibrary.org/search.json?q=${searchText}`);
@@ -14,7 +15,7 @@ function Visning({ books }) {
       console.error('Henting av data feilet', error);
     }
   };
-
+// i return har jeg laget mer eller mindre en HTML layout av nettsiden der jeg har brukt props fra Searchresult og home slik at de rendres ut
   return (
     <div className="visning">
       <header>
